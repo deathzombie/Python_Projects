3ca763d6bb8613ce28607018b6b2b4c71c2c05ee
Priyansh Saxena <priyanshsaxena@Priyanshs-MacBook-Air.local>
2023-04-10 22:44:43 +0530

    Madlib in Python

---
+++ Madlibs.py
@@ -0,0 +1,5 @@
+food = input("Enter a food article : ")
+book = input("Enter a book of choice :")
+verb = input("Enter an outdoor activity(with friends) : ")
+madlib = f"I like to eat {food}.\nReading of {book} really provided a deep insight in the life of mafia.\n{verb} is something i usually do around evening with bunch of my friends!"
+print(madlib)
